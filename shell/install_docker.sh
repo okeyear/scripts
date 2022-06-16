a1d32d30a6f6e44240cd09bffa6db4313ad0fd17
Jack Peng <okeyear@163.com>
2022-06-16 22:13:05 +0800

    install docker

---
+++ shell/install_docker.sh
@@ -0,0 +1,35 @@
+
+# install
+curl -fsSL get.docker.com -o get-docker.sh
+sudo sh get-docker.sh --mirror Aliyun
+
+# rootless user 
+sudo usermod -aG docker $USER
+# 如果已经sudo到root,请执行如下
+sudo usermod -aG docker $SUDO_USER
+systemctl start docker
+systemctl enable docker
+systemctl is-enabled docker
+docker version
+
+
+# speed up ,  docker register
+sudo mkdir -p /etc/docker
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": [
+    "http://hub-mirror.c.163.com",
+    "https://registry.docker-cn.com",
+    "https://xc8hlpxv.mirror.aliyuncs.com",
+    "http://f1361db2.m.daocloud.io"
+  ],
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  }
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl enable --now docker
+sudo systemctl restart docker
