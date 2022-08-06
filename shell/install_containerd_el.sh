2c4903ef5797ef54127792882863ac1ec72f1034
Jack Peng <okeyear@163.com>
2022-08-06 22:58:01 +0800

    Update install_containerd_el.sh

--- shell/install_containerd_el.sh
+++ shell/install_containerd_el.sh
@@ -3,39 +3,48 @@ export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export LANG=en
 set -e
 
+##############################
 # Step 1: Installing containerd
+
+# methond 1: install via yum
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+sudo yum install -y containerd.io
+
+# methond 2: install github release
 # download latest version containerd 
 function get_github_latest_release() {
     curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
     grep '"tag_name":' |                                            # Get tag line
     sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
 }
-containerd_ver=$(get_github_latest_release containerd/containerd)
-curl -SLO https://github.com/containerd/containerd/releases/download/$containerd_ver/containerd-${containerd_ver/v/}-linux-amd64.tar.gz
-# 
+# containerd_ver=$(get_github_latest_release containerd/containerd)
+# curl -SLO https://github.com/containerd/containerd/releases/download/$containerd_ver/containerd-${containerd_ver/v/}-linux-amd64.tar.gz
+
 # unzip the containerd
-sudo tar Cxzvf /usr/local -xf containerd-${containerd_ver/v/}-linux-amd64.tar.gz
+# sudo tar -C /usr/local -xf containerd-${containerd_ver/v/}-linux-amd64.tar.gz
 # create default config
 sudo mkdir /etc/containerd
 containerd config default | sudo tee /etc/containerd/config.toml
-# imageRepository sandbox_image 
-# registry.aliyuncs.com/google_containers
 
 # systemd service script
 # https://github.com/containerd/containerd/blob/main/containerd.service
-sudo mkdir -pv /usr/local/lib/systemd/system
-sudo curl https://raw.githubusercontent.com/containerd/containerd/main/containerd.service -o /usr/local/lib/systemd/system/containerd.service
-sudo systemctl daemon-reload
-sudo systemctl enable --now containerd.service
+# sudo mkdir -pv /usr/local/lib/systemd/system
+# sudo curl https://raw.githubusercontent.com/containerd/containerd/main/containerd.service -o /usr/local/lib/systemd/system/containerd.service
+# sudo systemctl daemon-reload
+# sudo systemctl enable --now containerd.service
 
+##############################
 # Step 2: Installing runc
 runc_ver=$(get_github_latest_release opencontainers/runc)
 curl -SLO https://github.com/opencontainers/runc/releases/download/${runc_ver}/runc.amd64
 sudo install -m 755 runc.amd64 /usr/local/sbin/runc
 
+##############################
 # Step 3: systemd cgroup driver
 sudo sed -i.bak '/SystemdCgroup/s/false/true/' /etc/containerd/config.toml
 
+##############################
 # Step 4: k8s containerd settings
 cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
@@ -52,7 +61,41 @@ EOF
 # 应用 sysctl 参数而无需重新启动
 sudo sysctl --system
 
-# Step 5: containerd proxy
+##############################
+# Step 5: genaral config settings
+
+# 配置containerd sandbox_image 
+# 问题, Error getting node" err="node not found" 
+# 解决办法, containerd配置中, 修改sandbox_image = "k8s.gcr.io/pause:3.6" 为可以拉取到的地址
+grep k8s.gcr.io /etc/containerd/config.toml
+sudo sed -i 's@k8s.gcr.io@registry.cn-hangzhou.aliyuncs.com/google_containers@' /etc/containerd/config.toml
+
+# above Step 4: systemd cgroup driver
+sudo sed -i.bak '/SystemdCgroup/s/false/true/' /etc/containerd/config.toml
+sudo grep containerd.runtimes.runc.options /etc/containerd/config.toml -A 20
+
+# 错误处理0.1
+grep cri /etc/containerd/config.toml
+# sed -i '/cri/s/^/#/' /etc/containerd/config.toml
+
+# 设置containerd的镜像加速 # imageRepository 
+grep 'registry.mirrors' -C 5 /etc/containerd/config.toml
+# 待更新: python toml模块安装之后的命令行去修改这些值, 比sed更好一点
+sed -i.bak '/registry.mirrors/a    [plugins."io.containerd.grpc.v1.cri".registry.mirrors."docker.io"]\n        endpoint = ["https://registry.docker-cn.com", "http://hub-mirror.c.163.com"]' /etc/containerd/config.toml
+
+# [plugins."io.containerd.grpc.v1.cri".registry.mirrors]
+# [plugins."io.containerd.grpc.v1.cri".registry.mirrors."docker.io"]
+#  endpoint = ["https://registry.docker-cn.com", "http://hub-mirror.c.163.com", "https://xc8hlpxv.mirror.aliyuncs.com"]
+
+
+sudo systemctl daemon-reload
+sudo systemctl enable --now containerd.service
+# sudo systemctl status containerd.service
+# sudo systemd-run -p Delegate=yes -p KillMode=process /usr/local/bin/containerd
+
+
+##############################
+# Step 6: containerd proxy
 # # set proxy for containerd
 # sudo mkdir -pv /etc/systemd/system/containerd.service.d
 # sudo tee /etc/systemd/system/containerd.service.d/http_proxy.conf << EOF
