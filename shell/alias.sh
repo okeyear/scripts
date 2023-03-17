2775b194ebb2dacb9a96b37187f7abb5b8874ddc
Jack Peng <okeyear@163.com>
2023-03-17 11:44:07 +0800

    Update alias.sh

--- shell/alias.sh
+++ shell/alias.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export LANG=en
+
+
 sudo yum install -y highlight
 alias cats='highlight -O ansi --syntax=bash'
 echo "alias cats='highlight -O ansi --syntax=bash'"  | sudo tee /etc/profile.d/alias.sh
