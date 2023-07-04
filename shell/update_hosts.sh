3f6e17add55e5e7fa3d331768e2cc3c944f47bae
Jack Peng <okeyear@163.com>
2023-07-04 16:31:45 +0800

    Update update_hosts.sh

--- shell/update_hosts.sh
+++ shell/update_hosts.sh
@@ -6,7 +6,7 @@ set -e
 sudo tee /etc/update_hosts.sh <<EOF
 echo '127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4' | sudo tee /etc/hosts
 echo '::1         localhost localhost.localdomain localhost6 localhost6.localdomain6' | sudo tee -a /etc/hosts
-curl -s https://gitee.com/fliu2476/github-hosts/raw/main/hosts | sudo tee -a /etc/hosts
+curl -s https://gitlab.com/ineo6/hosts/-/raw/master/next-hosts | sudo tee -a /etc/hosts
 EOF
 
 echo '0 */1 * * * sudo bash /etc/update_hosts.sh' | sudo tee -a /etc/crontab
